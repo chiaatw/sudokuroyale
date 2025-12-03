722d6d68c5a3e89cc512c5a2a1cecef6c46f1440
chiaatw <clbrehm@live.de>
2025-12-03 22:26:10 +0100

    update shape.rs

--- layout/shape.rs
+++ layout/shape.rs
@@ -14,3 +14,25 @@ pub trait ShapeTrait{
     fn houses(&self, house: Coord) -> CellSet;
     fn house(&self, house: Coord, index: usize) -> House;
 }
+
+impl ShapeTrait for Form {
+    fn label(&self) -> &str {
+        match self {
+            Form::Row => "Row",
+            Form::Column => "Col",
+            Form::Block => "Box",
+        }
+    }
+
+    fn index(&self) -> usize {
+        *self as usize
+    }
+
+    fn houses(&self, house: Coord) -> CellSet {
+        CELL_SETS[self.index()][house.usize()]
+    }
+
+    fn house(&self, house: Coord, index: usize) -> House {
+        House::new(*self, house)
+    }
+}
