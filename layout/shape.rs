e2b445b9b55da3ad6cc5bc6f6d57876a1c33f6d3
chiaatw <clbrehm@live.de>
2025-12-03 22:25:19 +0100

    update shape.rs

--- layout/shape.rs
+++ layout/shape.rs
@@ -8,42 +8,9 @@ pub enum Form {
     Block,
 }
 
-impl Form {
-    pub fn iter() -> FormIter {
-        FormIter::new()
-    }
-
-    pub const fn new(index: u8) -> Self {
-        debug_assert!(index <= 2);
-        match index {
-            0 => Self::Row,
-            1 => Self::Column,
-            2 => Self::Block,
-            _ => unreachable!(),
-        }
-    }
-
-    pub const fn usize(&self) -> usize {
-        *self as usize
-    }
-
-    pub const fn label(&self) -> &str {
-        match self {
-            Form::Row => "Row",
-            Form::Column => "Col",
-            Form::Block => "Box",
-        }
-    }
-
-    pub const fn is_row(&self) -> bool {
-        matches!(self, Form::Row)
-    }
-
-    pub const fn is_column(&self) -> bool {
-        matches!(self, Form::Column)
-    }
-
-    pub const fn is_block(&self) -> bool {
-        matches!(self, Form::Block)
-    }
-};
+pub trait ShapeTrait{
+    fn label(&self) -> &str;
+    fn index(&self) -> usize;
+    fn houses(&self, house: Coord) -> CellSet;
+    fn house(&self, house: Coord, index: usize) -> House;
+}
