b8e8b70dd9c198fb4d6baee25c5e0c029816d0a5
BenShaladotcom <benishala13@gmail.com>
2025-12-04 13:11:04 +0100

    Update coord.rs

--- layout/coord.rs
+++ layout/coord.rs
@@ -1,23 +1,20 @@
 use std::fmt;
 
-/// 9 Koordinaten (0–8), identisch zur ursprünglichen Logik.
 #[derive(
     Clone, Copy, Debug, Default, Hash,
     Eq, PartialEq, Ord, PartialOrd
 )]
+
 pub enum Coord {
     #[default] C0,
     C1, C2, C3,
     C4, C5, C6,
-    C7, C8,
+    C7, C8, 
 }
 
 impl Coord {
     pub const COUNT: u8 = 9;
 
-    /* ------------------------------
-       Interner Index (0–8)
-       ------------------------------ */
     pub const fn index(self) -> u8 {
         match self {
             Coord::C0 => 0,
@@ -32,9 +29,7 @@ impl Coord {
         }
     }
 
-    /* ------------------------------
-       Konstruktoren (gleich wie vorher)
-       ------------------------------ */
+
 
     pub const fn new(coord: u8) -> Self {
         debug_assert!(coord < 9);
@@ -43,28 +38,24 @@ impl Coord {
 
     pub const fn from_digit(digit: u8) -> Self {
         debug_assert!((1..=9).contains(&digit));
-        Self::from_index(digit - 1)
+        Self::from_index(digit -1)
     }
 
     pub const fn from_index(index: u32) -> Self {
-        debug_assert!(index < 9);
+        debug_assert!(index <9);
         match index {
-            0 => Coord::C0,
-            1 => Coord::C1,
-            2 => Coord::C2,
-            3 => Coord::C3,
-            4 => Coord::C4,
-            5 => Coord::C5,
-            6 => Coord::C6,
-            7 => Coord::C7,
-            8 => Coord::C8,
+            0 => Coord::C0;
+            1 => Coord::C1;
+            2 => Coord::C2;
+            3 => Coord::C3;
+            4 => Coord::C4;
+            5 => Coord::C5;
+            6 => Coord::C6;
+            7 => Coord::C7;
+            8 => Coord::C8;
             _ => unreachable!(),
         }
     }
-
-    /* ------------------------------
-       Getter (gleich wie vorher)
-       ------------------------------ */
 
     pub const fn u8(&self) -> u8 {
         self.index()
@@ -78,77 +69,56 @@ impl Coord {
         1 << self.index()
     }
 
-    pub const fn label(&self) -> char {
-        (b'1' + self.index()) as char
+    pub const fn label (&self) -> char {
+        (b'1' + self.index()) as char 
     }
 
-    /* ------------------------------
-       min / max wie im Original
-       ------------------------------ */
-
-    pub const fn min(self, other: Self) -> Self {
+    pub const fn min( self, other: Self ) -> Self {
         if self.index() <= other.index() { self } else { other }
     }
 
     pub const fn max(self, other: Self) -> Self {
         if self.index() >= other.index() { self } else { other }
     }
-}
 
-/* ------------------------------
-   From-Implementierungen
-   ------------------------------ */
-
-impl From<i32> for Coord {
-    fn from(coord: i32) -> Self {
-        debug_assert!(coord >= 0);
-        Coord::new(coord as u8)
+    impl From <i32> for Coord {
+        fn from(coord: i32) -> Self {
+            debug_assert! (coord >=0);
+            Coord::new(coord as u8)
+        }
     }
-}
-
-impl From<u8> for Coord {
-    fn from(coord: u8) -> Self {
-        Coord::new(coord)
+    impl From <u8> for Coord {
+        fn from(coord: u8) -> Self {
+            Coord::newcoord)
+        }
     }
-}
 
-impl From<usize> for Coord {
-    fn from(coord: usize) -> Self {
-        Coord::new(coord as u8)
+    impl From <usize> for Coord {
+        fn from(coord: usize) -> Self {
+            Coord::new(coord as u8)
+        }
     }
-}
-
-impl From<char> for Coord {
-    fn from(coord: char) -> Self {
-        Coord::new(coord as u8 - b'1')
+    impl From <char> for Coord {
+        fn from (coord: char) -> Self {
+            Coord::new(coord as u8 - b'1')
+        }
     }
-}
-
-impl From<&str> for Coord {
-    fn from(label: &str) -> Self {
-        Coord::from(label.chars().next().unwrap())
+    impl From<&str> for Coord {
+        fn from(label: &str) -> Self {
+            Coord::from(label.chars().next().unwrap())
+        }
     }
-}
-
-/* ------------------------------
-   Display wie im Original
-   ------------------------------ */
-
-impl fmt::Display for Coord {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.label())
+    impl fmt::Display for Coord {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{}", self.label())
+        }
     }
-}
+    macro_rules! coord {
+        ($c:expr) => {
+            Coord::new($c as u8 -1)
+        };
+    }
 
-/* ------------------------------
-   Makro 1:1 wie im Original
-   ------------------------------ */
+    pub(crate) use coord;
+    
 
-#[allow(unused_macros)]
-macro_rules! coord {
-    ($c:expr) => {
-        Coord::new($c as u8 - 1)
-    };
-}
-
-pub(crate) use coord;
