fb05eaeea98269dde9d66fa7c221e96112ec1e7c
chiaatw <clbrehm@live.de>
2025-12-07 12:54:55 +0100

    update house.rs

--- layout/houses/house.rs
+++ layout/houses/house.rs
@@ -6,7 +6,7 @@ pub trait HouseLike {
     fn cell(&self, coord: Coord) -> Cell;
 
     fn has(&self, cell: Cell) -> bool {
-        self.cells.has(cell)
+        self.cells().has(cell)
     }
 
     fn label(&self) -> &str;
@@ -24,6 +24,35 @@ pub struct Column {
     coord: Coord,
 }
 
-pub struct {
+pub struct Block{
     coord: Coord,
+}
+
+impl HouseLike for Row {
+    fn coord(&self) -> Coord { self.coord }
+    fn shape(&self) -> Shape { Shape::Row }
+
+    fn cells(&self) -> CellSet {
+        ROW_CELLS[self.coord.usize()]
+    }
+
+    fn cell(&self, coord: Coord) -> Cell {
+        Cell::from_row(self.coord, coord)
+    }
+
+    fn label(&self) -> &str { &LABELS[0][self.coord.usize()]}
+    fn console_label(&self) -> char { CONSOLE_LABELS[0][self.coord.usize()]}
+
+    fn intersect(&self, other: &dyn HouseLike) -> CellSet {
+        self.cells().intersect(other.cells())
+    }
+    fn crossing_houses(&self, cells: CellSet) -> HouseSet {
+        cells.iter().fold(HouseSet::empty(Shape::Column), |acc, c| acc + c.column_coord())
+    }
+}
+
+pub trait HouseIterator {
+    type Item: HouseLike;
+
+    fn iter() -> Box<dyn Iterator<Item = Self::Item>>;
 }
