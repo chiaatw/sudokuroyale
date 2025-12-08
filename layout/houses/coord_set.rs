ddbe8098baeddbe04ad6f670e6f17d5f365d967a
BenShaladotcom <benishala13@gmail.com>
2025-12-08 20:08:22 +0100

    Update coord_set.rs

--- layout/houses/coord_set.rs
+++ layout/houses/coord_set.rs
@@ -44,4 +44,47 @@ fn bits mut (&mut self) -> &mut Bits {
     debug_assert!(bits <= ALL_SET);
     CoordSet::Bits(bits)
   }
+  pub const fn from_coord(coord: Coord) -> Self {
+    CoordSet::Bits(coord.bit())
+  }
+  puvb const fn from_labels(labels: &str) - > Self {
+    let bytes = labels.as_bytes();
+    let mut bits: Bits = 0;
+    let mut i = 0;
+
+    while i < bytes.len() {
+      let c = bytes [i] as char;
+      debug_assert!('1' <= c && c <= '9');
+      bits |= 1 << (c as Size - b'1');
+      i+= 1;
+    }
+CoordSet::Bits(bits)
+}
+
+pub const fn from_coords(mut coords: i32) -> Self {
+let mut bits: Bits = 0;
+
+  while coords > 0 {
+    let c = coords % 10;
+    coords /= 10;
+    bits |= 1 << (c-1);
+  }
+  CoordSet::Bits(bits)
+}
+
+pub const fn bits (&self) -> Bits {
+  self.bits_raw()
+}
+pub const fn is_empty(&self) -> bool {
+  self.bits_raw() == 0
+}
+pub const fn is_full(&self) -> bool {
+  self.bits_raw() == ALL_SET
+}
+pub const fn len(&self) -> usize {
+  self.bits_raw() & coord.bit() != 0
+}
+
+    
   
+
