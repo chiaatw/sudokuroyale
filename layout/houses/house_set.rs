9b7cc2a13529e39d16d8088afc2f317c0808669d
chiaatw <clbrehm@live.de>
2025-12-13 12:54:52 +0100

    update houses_set.rs

--- layout/houses/house_set.rs
+++ layout/houses/house_set.rs
@@ -1,3 +1,14 @@
+use std::fmt;
+use std::iter::FusedIterator;
+use std::ops::{
+    Add, AddAssign, BitAnd, BitAndAssign, BitOr, BitOrAssign, Index, Not, Sub, SubAssign,
+};
+
+use crate::layout::CellSet;
+use crate::symbols::EMPTY_SET;
+
+use super::{Coord, CoordSet, House, Shape};
+
 pub trait HouseSetLike: Copy + Sized {
 
     const fn empty(shape: Shape) -> Self;
@@ -7,10 +18,213 @@ pub trait HouseSetLike: Copy + Sized {
 
     fn shape(&self) -> Shape;
     fn is_empty(&self) -> bool;
+    fn is_full(&self) -> bool;
     fn len(&self) -> usize;
 
     fn has_coord(&self, coord: Coord) -> bool;
     fn union(self, other: Self) -> Self;
     fn intersect(self, other: Self) -> Self;
     fn minus(self, other: Self) -> Self;
+    fn inverted(self) -> Self;
 }
+
+#[derive(Clone, Copy, Default, Hash, Eq, PartialEq, Ord, PartialOrd)]
+pub struct HouseSet{
+    shape: Shape,
+    coords: CoordSet,
+}
+
+impl HouseSetLike for HouseSet {
+
+    const fn empty(shape: Shape) -> Self {
+        Self {
+            shape,
+            coords: CoordSet::empty(),
+        }
+    }
+
+    const fn full(shape: Shape) -> Self {
+        Self {
+            shape,
+            coords: CoordSet::full(),
+        }
+    }
+
+    const fn from_bits(shape: Shape, bits: u16) -> Self {
+        Self {
+            shape,
+            coords: CoordSet::new(bits),
+        }
+    }
+
+    const fn from_coords(shape: Shape, coords: i32) -> Self {
+        Self {
+            shape,
+            coords: CoordSet::from_coords(coords),
+        }
+    }
+
+    fn shape(&self) -> Shape {
+        self.shape
+    }
+
+    fn is_empty(&self) -> bool {
+        self.coord,is_empty()
+    }
+
+    fn is_full(&self) -> bool {
+        self.coords.is_full()
+    }
+
+    fn len(&self) -> usize {
+        self.coords.len()
+    }
+
+    fn has_coord(&self, coord: Coord) -> bool {
+        self.coords.has(coord)
+    }
+
+    fn union(self, other: Self) -> Self {
+        if self.shape != other.shape {
+            panic!("Cannot union {} and {}", self.shape, other.shape);
+        }
+        Self {
+            shape: self.shape,
+            coords: self.coords | other.coords,
+        }
+    }
+
+    fn intersect(self, other: Self) -> Self {
+        if self.shape != other.shape {
+            panic!("Cannot intersect {} and {}", self.shape, other.shape);
+        }
+        Self {
+            shape: self.shape,
+            coords: self.coords & other.coords,
+        }
+    }
+
+    fn minus(self, other: Self) -> Self {
+        if self.shape != other.shape {
+            panic!("Cannot subtract {} and {}", self.shape, other.shape);
+        }
+        Self {
+            shape: self.shape,
+            coords: self.coords & !other.coords,
+        }
+    }
+
+    fn inverted(self) -> Self {
+        Self {
+            shape: self.shape,
+            coords: !self.coords,
+        }
+    }
+}
+
+impl HouseSet {
+
+    pub fn has(&self, house: House) -> bool {
+        if self.shape != house.shape() {
+            panic!("{} cannot be in {} set", house, self.shape);
+        }
+        self.coords.has(house.coord())
+    }
+
+    pub fn with(&self, house: House) -> Self {
+        if self.shape != house.shape() {
+            panic!("Cannot add {} to {} set", house, self.shape);
+        }
+        Self {
+            shape: self.shape,
+            coords: self.coords.with(house.coord())
+        }
+    }
+
+    pub fn without(&self, house: House) -> Self {
+        if self.shape != house.shape() {
+            panic!("Cannot remove {} from {} set", house, self.shape);
+        }
+        Self {
+            shape: self.shape,
+            coords: self.coords.without(house.coord()),
+        }
+    }
+
+    pub fn cells(&self) -> CellSet {
+        self.iter().fold(CellSet::empty(), | acc, h| acc | h.cells())
+    }
+
+    pub const fn iter(&self) -> Iter {
+        Iter {
+            shape: self.shape,
+            coords: self.coords.bits(),
+        }
+    }
+}
+
+impl Add<House> for HouseSet {
+    type Output = Self;
+    fn add(self, rhs: House) -> Self {
+        self.with(rhs)
+    }
+}
+
+impl Sub<House> for HouseSet {
+    type Output = Self;
+    fn sub(self, rhs: House) -> Self {
+        self.without(rhs)
+    }
+}
+
+impl BitOr for HouseSet {
+    type Output = Self;
+    fn bitor(self, rhs: Self) -> Self {
+        self.union(rhs)
+    }
+}
+
+impl BitAnd for HouseSet {
+    type Output = Self;
+    fn bitand(self, rhs: Self) -> Self {
+        self.minus(rhs)
+    }
+}
+
+impl Not for HouseSet {
+    type Output= Self;
+    fn not(self) -> Self {
+        self.inverted()
+    }
+}
+
+impl AddAssign<House> for HouseSet {
+    fn add_assign(&mut self, rhs: House) {
+        self = *self + rhs;
+    }
+}
+
+impl SubAssign<House> for HouseSet {
+    fn sub_assign(&mut self, rhs: House) {
+        *self = *self - rhs;
+    }
+}
+
+impl BitOrAssign for HouseSet {
+    fn bitor_assign(&mut self, rhs: Self) {
+        +self = *self | rhs;
+    }
+}
+
+impl BitAndAssign for HouseSet {
+    fn bitand_assign(&mut self, rhs: Self) {
+        *self = *self & rhs;
+    }
+}
+
+impl SubAssign for HouseSet {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
