06f6cfc03cb76770abf66f890b6bb8cc426b143b
chiaatw <clbrehm@live.de>
2025-12-18 17:12:34 +0100

    update

---
+++ src/solve/algorithms/peers.rs
@@ -0,0 +1,7 @@
+use super::*;
+
+/// Solver wrapper for the Peer (simple elimination) strategy
+/// 
+/// This allows the peer elimination logic to participate inn the unified solver pipeline alongside other strategies
+
+pub 
