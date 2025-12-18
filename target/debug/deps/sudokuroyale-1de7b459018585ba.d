77b589f1994f9e7b2dbab7d8d7a4bcfc19284001
chiaatw <clbrehm@live.de>
2025-12-18 17:07:27 +0100

    u pdate

---
+++ target/debug/deps/sudokuroyale-1de7b459018585ba.d
@@ -0,0 +1,5 @@
+C:\Users\clbre\.rustup\Gamestruct\sudokuroyale\target\debug\deps\sudokuroyale-1de7b459018585ba.d: src\main.rs
+
+C:\Users\clbre\.rustup\Gamestruct\sudokuroyale\target\debug\deps\libsudokuroyale-1de7b459018585ba.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/sudokuroyale-de1e54d119db9063.d
@@ -0,0 +1,5 @@
+C:\Users\clbre\.rustup\Gamestruct\sudokuroyale\target\debug\deps\sudokuroyale-de1e54d119db9063.d: src\main.rs
+
+C:\Users\clbre\.rustup\Gamestruct\sudokuroyale\target\debug\deps\libsudokuroyale-de1e54d119db9063.rmeta: src\main.rs
+
+src\main.rs:
