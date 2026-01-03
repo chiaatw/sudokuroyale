938378d3337a792f36f23d7389da590f6c8b060b
chiaatw <clbrehm@live.de>
2026-01-03 18:59:25 +0100

    finished symbols.rs file

---
+++ src/symbols.rs
@@ -0,0 +1,10 @@
+pub const UNKNOWN_VALUE: char = '∘';
+pub const CANDIDATE: char = '⌾';
+
+pub const MISSING: char = '·';
+pub const GIVEN: char = '•';
+pub const EMPTY_SET: char = '∅';
+pub const EMPTY_SET_STR: char = "∅";
+
+pub const SET_KNOWN: char = '⇨';
+pub const REMOVE_CANDIDATE: char = '×';
