ef334d2a9e8f679b80c115cedcb89c7f5492298e
chiaatw <clbrehm@live.de>
2026-01-04 20:38:35 +0100

    included tests in bit.rs file

--- src/layout/cells/bit.rs
+++ src/layout/cells/bit.rs
@@ -64,3 +64,64 @@ impl From<Cell> for Bit {
         cell.bit()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Cell;
+
+    #[test]
+    fn test_bit_new_and_index() {
+        // Erstes Bit
+        let bit = Bit::new(1);
+        assert_eq!(bit.index(), 0);
+        assert_eq!(bit.bit(), 1);
+
+        // Bit an Position 5
+        let bit2 = Bit::new(1 << 5);
+        assert_eq!(bit2.index(), 5);
+        assert_eq!(bit2.bit(), 32);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bit_new_invalid() {
+        // Mehr als ein Bit gesetzt -> soll panic
+        Bit::new(3);
+    }
+
+    #[test]
+    fn test_bit_cell_conversion() {
+        let cell = Cell::new(7);
+        let bit: Bit = cell.into();
+        assert_eq!(bit.index(), 7);
+        assert_eq!(bit.cell(), cell);
+    }
+
+    #[test]
+    fn test_bit_from_label() {
+        let bit = Bit::from("A1");
+        assert_eq!(bit.index(), 0);
+
+        let bit2 = Bit::from("C3");
+        let expected_index = index_from_label("C3");
+        assert_eq!(bit2.index(), expected_index);
+    }
+
+    #[test]
+    fn test_bit_max_and_all() {
+        assert_eq!(Bit::MAX, 1 << (Cell::COUNT - 1));
+        assert_eq!(Bit::ALL, (1 << Cell::COUNT) - 1);
+    }
+
+    #[test]
+    fn test_bit_deref() {
+        let mut bit = Bit::new(1 << 4);
+        assert_eq!(*bit, 1 << 4);
+
+        // DerefMut testen
+        *bit = 1 << 5;
+        assert_eq!(bit.index(), 5);
+    }
+}
+
