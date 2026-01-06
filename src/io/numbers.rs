c477afe2007618efd2d041af2b87cbb42831022e
chiaatw <clbrehm@live.de>
2026-01-06 14:32:49 +0100

    included tests in numbers.rs file

--- src/io/numbers.rs
+++ src/io/numbers.rs
@@ -19,3 +19,42 @@ pub fn format_number(number: u128) -> String {
         .unwrap()
         .join(",")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::time::Duration;
+
+    #[test]
+    fn test_format_number_small() {
+        assert_eq!(format_number(0), "0");
+        assert_eq!(format_number(5), "5");
+        assert_eq!(format_number(12), "12");
+        assert_eq!(format_number(123), "123");
+    }
+
+    #[test]
+    fn test_format_number_thousands() {
+        assert_eq!(format_number(1234), "1,234");
+        assert_eq!(format_number(12345), "12,345");
+        assert_eq!(format_number(123456), "123,456");
+    }
+
+    #[test]
+    fn test_format_number_millions() {
+        assert_eq!(format_number(1234567), "1,234,567");
+        assert_eq!(format_number(12345678), "12,345,678");
+        assert_eq!(format_number(123456789), "123,456,789");
+    }
+
+    #[test]
+    fn test_format_runtime() {
+        let d1 = Duration::from_micros(0);
+        let d2 = Duration::from_micros(1234);
+        let d3 = Duration::from_micros(1_234_567);
+
+        assert_eq!(format_runtime(d1), "0");
+        assert_eq!(format_runtime(d2), "1,234");
+        assert_eq!(format_runtime(d3), "1,234,567");
+    }
+}
