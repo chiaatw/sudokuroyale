e361da4721a1b9eb85a09142fd25ca525e5f22e3
chiaatw <clbrehm@live.de>
2026-01-06 14:33:00 +0100

    included tests in progress.rs file

--- src/io/progress.rs
+++ src/io/progress.rs
@@ -7,3 +7,30 @@ const EMPTY: &str =
 pub fn show_progress(size: usize) {
     println!("{}{}", &FILLED[..size + 1], &EMPTY[size + 1..]);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_progress_bar_start() {
+        let result = progress_bar(0);
+        assert_eq!(result.len(), FILLED.len());
+        assert_eq!(&result[..1], &FILLED[..1]);
+    }
+
+    #[test]
+    fn test_progress_bar_middle() {
+        let size = 10;
+        let result = progress_bar(size);
+        assert_eq!(&result[..size + 1], &FILLED[..size + 1]);
+        assert_eq!(&result[size + 1..], &EMPTY[size + 1..]);
+    }
+
+    #[test]
+    fn test_progress_bar_full() {
+        let size = FILLED.len() - 1;
+        let result = progress_bar(size);
+        assert_eq!(result, FILLED);
+    }
+}
