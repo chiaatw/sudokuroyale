c13a4f6c09e34c2f19aaeac762379ab5acd87711
chiaatw <clbrehm@live.de>
2026-01-14 11:38:11 +0100

    update

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -7,6 +7,6 @@ pub mod sudoku;
 
 pub use cells::{Bit, Cell, CellSet, Label, Rectangle};
 pub use houses::{Coord, CoordSet, House, HouseSet, Shape};
-pub use values::{Known, KnownSet, Value};
+pub use values::{Known, KnownSet, Value, ValueLike};
 pub use grid::Grid;
 pub use sudoku::Sudoku;
