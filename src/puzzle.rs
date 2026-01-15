88434c219083019da271eb8fa653a69d2f6649ea
chiaatw <clbrehm@live.de>
2026-01-15 18:44:47 +0100

    add puzzle io files

---
+++ src/puzzle.rs
@@ -0,0 +1,6 @@
+pub use crate::layout::{Cell, CellSet, Known, KnownSet, Value, ValueLike};
+
+pub use crate::solve::strategy_ord::{
+    Action, Board, Change, Changer, ChangeResult, Clues, Difficulty, Effects, Error, Options,
+    Strategy, Timings, Verdict,
+};
